de556b8fa5edced27800aff0d7aa68b044a995db
ailtonnetodev <ailtonnetto8319@gmail.com>
2023-12-07 20:56:51 -0300

    Criando um gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,14 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 import { User } from "./User"
 
 Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
 Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 Course.hasMany(Episode, { as: 'episodes'})
 
@@ -17,12 +19,14 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
     Category,
     Course,
     Episode,
-    Favorite,  
+    Favorite,
+    Like,
     User
 }           
---
+++ src/services/likeService.ts
@@ -0,0 +1,14 @@
+import { Like } from "../models"
+
+export const likeService = {
+  create: async (userId: number, courseId: number) => {
+    const like = await Like.create({
+      userId,
+      courseId
+    })
+
+    return like
+  }
+}
+
+
