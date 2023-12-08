4f4461470905af0d34d59906d8752e29a745540c
ailtonnetodev <ailtonnetto8319@gmail.com>
2023-12-07 21:23:35 -0300

    Atualizando o endpoint de cursos

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -17,6 +17,17 @@ export const likeService = {
         courseId
       }
     })
+  },
+
+  isLiked: async (userId: number, courseId: number) => {
+    const like = await Like.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return like !== null ? true : false
   }
 }
 
